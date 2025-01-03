b4d6a6f6bea0892aec18bc6f5c50f21103bd0042
Nick Youngblut <nyoungb2@gmail.com>
2025-01-03 16:56:56 +0000

    updated cloud run job entrypoint to create working directory

--- docker/sc-recounter-run/entrypoint.sh
+++ docker/sc-recounter-run/entrypoint.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+# create run name
+RUN_NAME="SCRECOUNTER_$(date +"%Y-%m-%d_%H-%M-%S")"
+
 # Activate the micromamba environment and run the pipeline
 micromamba run -n sc-recounter-run \
   nextflow run main.nf \
-    -profile docker,gcp,dev,no_acc_dev,report,trace \
+    -profile docker,gcp,prod,report,trace \
+    -name $RUN_NAME \
+    -work-dir gs://arc-ctc-nextflow/scRecounter/prod/work/${RUN_NAME} \
     -ansi-log false "$@"
