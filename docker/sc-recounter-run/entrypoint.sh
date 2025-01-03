29c363f2646bf001b70c4a20fc6db512c64f9fdf
Nick Youngblut <nyoungb2@gmail.com>
2025-01-03 03:20:39 +0000

    added cleanup for prod profile

--- docker/sc-recounter-run/entrypoint.sh
+++ docker/sc-recounter-run/entrypoint.sh
@@ -3,4 +3,5 @@
 # Activate the micromamba environment and run the pipeline
 micromamba run -n sc-recounter-run \
   nextflow run main.nf \
-    -profile docker,gcp,dev,no_acc_dev,report,trace -ansi-log false "$@"
+    -profile docker,gcp,dev,no_acc_dev,report,trace \
+    -ansi-log false "$@"
