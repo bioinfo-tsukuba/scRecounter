43b5711633e5dd3e5b52576aca67e324530b9163
nick-youngblut <nyoungb2@gmail.com>
2025-02-01 09:04:29 -0800

    Enhance search-cloud-run-job-logs.py with additional arguments for job name, severity, and content output; update log retrieval logic and improve command execution feedback.

--- scripts/search-cloud-run-job-logs.py
+++ scripts/search-cloud-run-job-logs.py
@@ -3,8 +3,10 @@ import sys
 import json
 import argparse
 import subprocess
+from typing import Optional
 from datetime import datetime, timedelta, timezone
 import pytz
+import pandas as pd
 
 
 class CustomFormatter(argparse.ArgumentDefaultsHelpFormatter, argparse.RawDescriptionHelpFormatter): pass
@@ -12,22 +14,35 @@ class CustomFormatter(argparse.ArgumentDefaultsHelpFormatter, argparse.RawDescri
 def parse_args() -> argparse.Namespace:
     desc = 'Search for logs in Cloud Run Jobs that contain a specific keyword.'
     epi = """DESCRIPTION:
-    Search for logs in Cloud Run Jobs that contain a specific keyword, starting from a given datetime.
-    Example: search-cloud-run-job-logs.py "ALREADY_EXISTS" 
+    Search for logs in Cloud Run Jobs.
+    Examples:
+      $ search-cloud-run-job-logs.py --keyword "ALREADY_EXISTS" 
+      $ search-cloud-run-job-logs.py --content
     """
-    # default datetime of 1 day ago
+    # default datetime of N day ago
     default_datetime = (datetime.now(timezone.utc) - timedelta(days=3)).strftime("%Y-%m-%dT%H:%M:%SZ")
 
     parser = argparse.ArgumentParser(description=desc, epilog=epi, formatter_class=CustomFormatter)
     parser.add_argument(
-        "keyword", help="The keyword to search for in Cloud Run Job logs."
+        "-k", "--keyword", default=None, help="The keyword to search for in Cloud Run Job logs."
     )
     parser.add_argument(
-        "--project-id", default="c-tc-429521", help="The Google Cloud project ID."
+        "-p", "--project-id", default="c-tc-429521", help="The Google Cloud project ID."
+    )
+    parser.add_argument(
+        "-n", "--job-name", default="sc-recounter-run", help="The name of the Cloud Run Job."
     )
     parser.add_argument(
         "--start-datetime", type=str, default=default_datetime,
-        help="Start datetime for logs in ISO 8601 format (e.g., 2025-01-15T00:00:00Z)."
+        help="Start datetime for logs in ISO 8601 format."
+    )
+    parser.add_argument(
+        "--severity", type=str, default="ERROR", 
+        help="The minimum severity level of the logs to retrieve."
+    )
+    parser.add_argument(
+        "--content", action="store_true", default=False,
+        help="Print the content of the logs."
     )
     parser.add_argument(
         "--limit", type=int, default=None, 
@@ -47,22 +62,45 @@ def convert_time(timestamp: str) -> str:
         timestamp_str = f"Error converting time: {e}"
     return timestamp_str
 
-def find_logs(keyword: str, project_id: str, start_datetime: str, limit: int=None) -> None:
+def find_logs(
+        project_id: str, 
+        start_datetime: str, 
+        job_name: str="sc-recounter-run", 
+        region: str="us-east1",
+        keyword: Optional[str]=None, 
+        severity: Optional[str]="ERROR",
+        limit: int=None,
+    ) -> None:
+    """
+    Find logs in Cloud Run Jobs 
+    """
     next_page_token = None
     logs_retrieved = 0
 
     # add 8 hours to start_datetime to account for the difference between GMT and PCT
     start_datetime = (datetime.fromisoformat(start_datetime) + timedelta(hours=8)).strftime("%Y-%m-%dT%H:%M:%SZ")
 
+
+    job_info = []
     while True:
         # Construct the gcloud command
-        cmd = f'gcloud logging read \'resource.type=cloud_run_job AND textPayload:{keyword} AND timestamp>="{start_datetime}"\''
-        cmd += f" --project={project_id} --format=json --limit=1000"
-        print(cmd); 
+        query = [
+            'resource.type="cloud_run_job"',
+            f'resource.labels.job_name="{job_name}"',
+            f'resource.labels.location="{region}"',
+            f'timestamp>="{start_datetime}"',
+        ]
+        if severity:
+            query.append(f'severity>={severity}')
+        if keyword:
+            query.append(f'textPayload:{keyword}')
+        query = " AND ".join(query)
+        cmd = f"gcloud logging read '{query}' --project={project_id} --format=json --limit=1000"
         if next_page_token:
             cmd += f" --page-token={next_page_token}"
-        print(f"Executing command: {cmd}", file=sys.stderr)
 
+        # Execute the gcloud command
+        print(f"Executing command: {cmd}", file=sys.stderr)
         try:
             result = subprocess.run(cmd, shell=True, capture_output=True, text=True, check=True)
             logs = json.loads(result.stdout)
@@ -74,26 +112,69 @@ def find_logs(keyword: str, project_id: str, start_datetime: str, limit: int=Non
                 job_name = log.get("resource", {}).get("labels", {}).get("job_name", "Unknown")
                 execution_id = log.get("labels", {}).get("run.googleapis.com/execution_name", "Unknown")
                 timestamp = convert_time(log.get("timestamp", "Unknown"))
-                print(f"Job Name: {job_name}, Execution ID: {execution_id}, Timestamp: {timestamp}")
+                #print(f"Job Name: {job_name}, Execution ID: {execution_id}, Timestamp: {timestamp}")
+                job_info.append([job_name, execution_id, timestamp])
                 logs_retrieved += 1
 
                 # Stop if we've reached the limit
                 if limit and logs_retrieved >= limit:
                     print(f"Reached the limit of {limit} logs.")
-                    return
+                    return job_info
 
             # Check if there's a next page token
             next_page_token = result.stderr.split("nextPageToken: ")[-1].strip() if "nextPageToken" in result.stderr else None
             if not next_page_token:
                 break
-
         except subprocess.CalledProcessError as e:
             print(f"Error executing gcloud command: {e.stderr.strip()}")
             break
         except json.JSONDecodeError:
             print("Failed to parse the JSON response from gcloud.")
             break
+    return job_info
+
+def get_content(
+    log_info: pd.DataFrame,
+    project_id: str,
+) -> None:
+    for index, row in log_info.iterrows():
+        job_name = row["Job Name"]
+        execution_id = row["Execution ID"]
+        query = f'labels."run.googleapis.com/execution_name"="{execution_id}"'
+        cmd = f"gcloud logging read '{query}' --project={project_id} --format=json"
+        print(f"Executing command: {cmd}", file=sys.stderr)
+        try:
+            result = subprocess.run(cmd, shell=True, capture_output=True, text=True, check=True)
+            logs = json.loads(result.stdout)
+            print(f"#-- Execution ID: {execution_id} --#")
+            for log in logs:
+                print(log.get("textPayload", ""))
+        except subprocess.CalledProcessError as e:
+            print(f"Error executing gcloud command: {e.stderr.strip()}")
+        except json.JSONDecodeError:
+            print("Failed to parse the JSON response from gcloud.")
 
 if __name__ == "__main__":
     args = parse_args()
-    find_logs(args.keyword, args.project_id, args.start_datetime, args.limit)
+
+    # find the logs
+    log_info = find_logs(
+        keyword=args.keyword, 
+        project_id=args.project_id,
+        start_datetime=args.start_datetime, 
+        job_name=args.job_name,
+        severity=args.severity,
+        limit=args.limit,
+    )
+    # convert to a pandas dataframe
+    log_info = pd.DataFrame(log_info, columns=["Job Name", "Execution ID", "Timestamp"])
+
+    # get the content of the logs or save general log info to a CSV file
+    if args.content:
+        # get the content of the logs
+        get_content(log_info, args.project_id)
+    else:
+        # save to a CSV file
+        log_info.to_csv(sys.stdout, index=False)
+
+        
